bc815504c92999f6a0ccd33f8797de8805662d22
LZWinnie <41434721+LZWinnie@users.noreply.github.com>
2018-07-19 20:59:51 +0800

    zz

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     Widget w;
-    w.setWindowTitle("QPTZcontrol by Peter_White");
+    w.setWindowTitle("QPTZcontrol by Peter_White & ZZ");
     w.show();
     w.ConsoleAppend("*****System Start*****\n");
 
